1d39eef17c9002b12fa5fb09df3b2919b12e166c
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2022-06-01 15:36:41 -0400

    Scaffolding for processed audio bundle class.

---
+++ src/Entity/ProcessedAudio.php
@@ -0,0 +1,13 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Drupal\processed_audio_entity\Entity;
+
+use Drupal\media\Entity\Media;
+
+/**
+ * Represents a "processed audio" bundle of a media entity.
+ */
+class ProcessedAudio extends Media {
+}
