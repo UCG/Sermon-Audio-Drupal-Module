e509789e5a3bcfb06378f9f76debf8c8da4566f6
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2022-06-15 13:40:03 -0400

    Sermon audio doc.

--- src/Entity/SermonAudio.php
+++ src/Entity/SermonAudio.php
@@ -9,6 +9,19 @@ use Drupal\Core\Entity\ContentEntityBase;
 /**
  * An entity representing audio for a sermon.
  *
+ * Audio processing of the "unprocessed audio" field can be initiated with the
+ * intiateAudioProcessing() method. This method sets the
+ * field_audio_processing_initiated flag.
+ *
+ * After a "processed audio" entity is intially loaded (this does not occur on
+ * subsequent loads within the same request), a "post load" handler checks to
+ * see if audio_processing_intiated is set. If the field is set and the
+ * processed audio field is not set, a check is made to see if the AWS audio
+ * processing job has finished. If it has, the entity's "processed audio" field
+ * is updated with the processed audio file, and the entity is saved. The AWS
+ * audio processing job check and subsequent processed audio field update can
+ * also be forced by calling refreshProcessedAudio().
+ *
  * @ContentEntityType(
  *   id = "sermon_audio",
  *   label = @Translation("Sermon Audio"),
