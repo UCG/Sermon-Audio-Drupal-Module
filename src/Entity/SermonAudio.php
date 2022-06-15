ef3a3adfa221bf311684b9417f5b9e6274a7b5d1
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2022-06-15 13:40:15 -0400

    Sermon audio base field defns.

--- src/Entity/SermonAudio.php
+++ src/Entity/SermonAudio.php
@@ -5,6 +5,9 @@ declare (strict_types = 1);
 namespace Drupal\sermon_audio\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * An entity representing audio for a sermon.
@@ -37,4 +40,54 @@ use Drupal\Core\Entity\ContentEntityBase;
  * )
  */
 class SermonAudio extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) : array {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['duration'] = BaseFieldDefinition::create('float')
+      ->setLabel(new TranslatableMarkup('Duration'))
+      ->setDescription(new TranslatableMarkup('Duration of processed sermon audio.'))
+      ->setCardinality(1)
+      ->setTranslatable(TRUE)
+      ->setRequired(FALSE)
+      ->setSetting('unsigned', TRUE)
+      ->setSetting('min', 0);
+    $fields['processing_initiated'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(new TranslatableMarkup('Processing Initiated'))
+      ->setDescription(new TranslatableMarkup('Whether processing of the unprocessed audio has yet been initiated.'))
+      ->setCardinality(1)
+      ->setTranslatable(TRUE)
+      ->setRequired(FALSE)
+      ->setDefaultValue(FALSE)
+      ->setSetting('on_label', 'On')
+      ->setSetting('off_label', 'Off');
+    $fields['processed_audio'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(new TranslatableMarkup('Processed Audio'))
+      ->setDescription(new TranslatableMarkup('Processed audio media reference.'))
+      ->setCardinality(1)
+      ->setRequired(TRUE)
+      ->setTranslatable(TRUE)
+      ->setSetting('target_type', 'media')
+      // Per https://www.drupal.org/project/commerce/issues/3137225 and
+      // https://www.drupal.org/node/2576151, it seems target bundles should
+      // perhaps have identical keys and values.
+      ->setSetting('handler_settings', ['target_bundles' => ['audio' => 'audio']]);
+    $fields['unprocessed_audio'] = BaseFieldDefinition::create('file')
+      ->setLabel(new TranslatableMarkup('Unprocessed Audio'))
+      ->setDescription(new TranslatableMarkup('Unprocessed audio file.'))
+      ->setCardinality(1)
+      ->setRequired(TRUE)
+      ->setTranslatable(TRUE)
+      ->setSetting('target_type', 'file')
+      ->setSetting('uri_scheme', 's3')
+      ->setSetting('file_extensions', 'mp3 mp4 m4a wav aac ogg')
+      ->setSetting('max_filesize', '1 GB')
+      ->setSetting('file_directory', 'audio-uploads');
+
+    return $fields;
+  }
+
 }
