c9599c26ea8b536495251c74c22ecd912142131f
Jonathan Api <jonathan_api@ucg.org>
2026-01-08 12:11:12 -0500

    AT-39 - Removed unneccessary CoversMethod and TextDox lines.

--- tests/src/Unit/FinalTranscriptionGeneratorTest.php
+++ tests/src/Unit/FinalTranscriptionGeneratorTest.php
@@ -10,13 +10,11 @@ use Drupal\sermon_audio\FinalTranscriptionGenerator;
 use Drupal\Tests\sermon_audio\Traits\SampleTranscriptionDataTrait;
 use Drupal\Tests\UnitTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\CoversMethod;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\TestDox;
 use Ranine\Testing\Drupal\Traits\MockConfigFactoryCreationTrait;
 
 #[CoversClass(FinalTranscriptionGenerator::class)]
-#[CoversMethod('FinalTranscriptionGenerator','generate')]
 #[Group('sermon_audio')]
 class FinalTranscriptionGeneratorTest extends UnitTestCase {
 
